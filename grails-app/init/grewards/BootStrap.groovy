ccba667c53c29a728bda8120fd31cb67ecefd776
Renan Martins <martins@fujisan.co.jp>
2016-12-28 17:03:31 -0800

    Add initial data

--- grails-app/init/grewards/BootStrap.groovy
+++ grails-app/init/grewards/BootStrap.groovy
@@ -3,6 +3,9 @@ package grewards
 class BootStrap {
 
     def init = { servletContext ->
+    	new Product(name: "banana", sku: "BNN01", price: 0.29).save()
+    	new Product(name: "apple", sku: "APL01", price: 0.69).save()
+    	new Product(name: "cesar salad", sku: "CSL01", price: 3.99).save()
     }
     def destroy = {
     }
