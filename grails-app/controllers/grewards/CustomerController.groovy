00795f29d5468faa62e0f2adbae47d9352d6741d
Renan Martins <martins@fujisan.co.jp>
2016-12-29 15:59:39 -0800

    Add Customer#checkin action

--- grails-app/controllers/grewards/CustomerController.groovy
+++ grails-app/controllers/grewards/CustomerController.groovy
@@ -4,4 +4,10 @@ class CustomerController {
 	// allows scaffold (gives us CRUD automatically)
 	// in Grails 3, you need to give the domain class instead of just "true"
 	static scaffold = Customer
+
+	// Just by having this empty action will 
+	// cause the corresponding view to be rendered
+	def checkin() {
+
+	}
 }
